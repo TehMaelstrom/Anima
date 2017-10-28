1ac36392449d11ab468da584fc6ecc43b7d2cb58
Unknown <christmann.yoann@outlook.fr>
2017-10-28 12:46:46 +0200

    First commit
    Initializing webapp, domain and frontend projects

---
+++ anima-webapp/src/test/java/com/rpg/anima/animawebapp/AnimaWebappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rpg.anima.animawebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnimaWebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
